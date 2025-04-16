a1cfa29d9b68af968222c6a8c9a4c78febc42954
Torchinskij Alexandr <alekzz02@mail.ru>
2025-04-17 01:05:44 +0500

    First upset6

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -56,7 +56,7 @@ public class GlobalExceptionHandler {
 
     // Обработка MpaNotFoundException
     @ExceptionHandler(MpaNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleMpaNotFoundException(MpaNotFoundException ex) {
         return new ErrorResponse("Ошибка валидации", ex.getMessage());
     }
